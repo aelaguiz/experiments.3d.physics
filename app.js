75601a8f860c7854f8b7c7005060f06ed21a9ebf
Amir Elaguizy <aelaguiz@aelaguiz-desktop.(none)>
2011-02-22 11:10:21 -0600

    Fixed page title

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.configure('production', function(){
 app.get('/', function(req, res){
   res.render('index', {
     locals: {
-      title: 'Express'
+      title: 'Amir\'s 3d/physics experiments'
     }
   });
 });
